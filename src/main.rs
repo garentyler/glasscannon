35851a1a34e53dc20ce02e7377361b6e3e594960
Danton <43303409+khimno@users.noreply.github.com>
2020-09-09 08:36:38 -0600

    inertial kermit (bad code warning)

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs;
+static SRV_ID: &'static str = "GlassCannon/0.0.0";
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:15000").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        hc(stream);
+    }
+}
+
+fn hc(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        (sc("200".to_string()), "hello.html")
+    } else {
+      ("HTTP/1.1 405 METHOD NOT ALLOWED\r\n\r\n", "405.html")
+    };
+  let contents = fs::read_to_string(filename).unwrap();
+  let response = format!("{}{}", status_line, contents);
+
+  stream.write(response.as_bytes()).unwrap();
+  stream.flush().unwrap();
+}
+
+fn sc(code: String) ->&'static str{
+    if code.eq("200") {
+        return "HTTP/1.1 200 OK\r\ncontent-type: text/html\r\nserver: GlassCannon\r\n\r\n";
+    }
+    return "HTTP/1.1 400 BAD REQUEST\r\nserver: GlassCannon\r\n\r\n";
+}
